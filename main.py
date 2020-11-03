3770d317546bc2206451adcc7a58f7a5dba5092b
musavinejad <musavi.nezhad@yahoo.com>
2020-11-03 16:49:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ import scipy.ndimage as ndimage
 import scipy.ndimage.filters as filters
 from CryPy import *
 
-import matplotlib.patches as patches
-
 ############     constants:
 
 name = ""
@@ -25,26 +23,32 @@ threshold = 1500
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     folder = os.path.abspath(__file__)[:-7]
     os.chdir(folder)
     os.makedirs("DataOutPut", exist_ok=True)
 
-    name = select_data()
-    name = name[:-11]  # taking out "_imagem.cam"
+    name = select_data()  ## put the data you want to analyze in a folder named "...\Data\"
+    name = name[:-11]  ## taking out "_imagem.cam"
     print('analysing ', name[5:])
     reader = np.array(read_camera(location=name + '_imagem.cam', load_all=True))  # read data as np.array
+
+    data= pd.read_csv(name + '.dat', delimiter='\t', usecols=['DAQ_Index','ElapsedTime','Frequency'])
     mean_BG, STD_BG = measure_noise(reader, 40, 40)  # for the threshold
     max_img = get_Max_Trace(reader)
-        ### plotting the max trace image and the molecules found in it.
+    ### plotting the max trace image and the molecules found in it.
     fig, ax = plt.subplots(1)
     ax.imshow(max_img)
     os.chdir(folder + r'\DataOutput')
     fig.savefig(r'Maxtrace.png')
-    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=mean_BG + 3 * STD_BG)
-    for i in range(0, molx.size):
-        ax.add_patch(
-            patches.Rectangle([molx[0, i] - 2, moly[0, i] - 2], 4, 4, fill=False, edgecolor='white', linestyle='--'))
-        plt.text(molx[0, i] - 2, moly[0, i] - 2, '{}'.format(i), color='orange')
+    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=mean_BG + 1* STD_BG)
+    molx[1] = 63
+    moly[1] = 58
+    add_patches_to_img(ax, molx, moly)  ## Adds rectangles around the molecules
     fig.savefig(r'Maxtrace_withMolecules.png')
-        ### max trace image is saved. ,molx/y contains the coordinates of possible molecules
+    ### max trace image is saved. ,molx/y contains the coordinates of possible molecules
+
+    get_mols_spectrum(data, reader, molx, moly)
+
+    #data=data.sort_values(by=['Frequency'])
+    plot_mols(data, molx.size)
+
