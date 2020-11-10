6b469dba9e2a318e961662e0f48282d57c2c0dbe
musavinejad <musavi.nezhad@yahoo.com>
2020-11-10 19:45:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os.path
 from typing import Optional, List, Any, Union
 
 import pandas as pd
+from docutils.nodes import figure
 from matplotlib import pyplot as plt
 import numpy as np
 from pylablib.aux_libs.file_formats.cam import CamReader
@@ -12,10 +13,18 @@ import scipy.ndimage as ndimage
 import scipy.ndimage.filters as filters
 from CryPy import *
 import matplotlib
+from CryPy.OSfunc import *
+from CryPy.ExtractDiffusion import *
+from CryPy.SignalProcessing import *
 
+########################################
+## input documentation names :
 
+molname = '751-58nm_LPat780Tilted800_ND6_24mV_Fine500MHzps'
 
-############     constants:
+#########################################
+
+############     Local codes constants:
 
 name = ""
 neighborhood_size = 5
@@ -26,33 +35,49 @@ threshold = 1500
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    folder = os.path.abspath(__file__)[:-7]
-    os.chdir(folder)
-    os.makedirs("DataOutPut", exist_ok=True)
+
+    folder= prepare_folders(MolNameTag = molname)
 
     name = select_data()  ## put the data you want to analyze in a folder named "...\Data\"
-    name = name[:-11]  ## taking out "_imagem.cam"
-    print('analysing ', name[5:])
-    reader = np.array(read_camera(location=name + '_imagem.cam', load_all=True))  # read data as np.array
+    reader = read_camera(location=name + '_imagem.cam', load_all=False)
+    #reader = np.array(read_camera(location=name + '_imagem.cam', load_all=True))  # read data as np.array
 
     data= pd.read_csv(name + '.dat', delimiter='\t', usecols=['DAQ_Index','ElapsedTime','Frequency'])
-    mean_BG, STD_BG = measure_noise(reader, 40, 40)  # for the threshold
+    mean_BG, STD_BG = measure_noise(reader)  # for the threshold in finding molecules
     max_img = get_Max_Trace(reader)
     ### plotting the max trace image and the molecules found in it.
     fig, ax = plt.subplots(1)
-    ax.imshow(max_img)
-    os.chdir(folder + r'\DataOutput')
-    fig.savefig(r'Maxtrace.png')
-    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=6* STD_BG)
+    ax.imshow(max_img, vmin= mean_BG, vmax= mean_BG+10* STD_BG )
+    fig.suptitle(molname+ '_Max Trace')
+
+    fig.savefig(molname + '_DataOutPut\\' + molname + r'MaxTrace.png' )
+    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=4* STD_BG)
     #molx[1] = 46
     #moly[1] = 81
     #molx = np.array([82])
     #moly = np.array([70])
     add_patches_to_img(ax, molx, moly)  ## Adds rectangles around the molecules
-    fig.savefig(r'Maxtrace_withMolecules.png')
+    fig.savefig(molname + '_DataOutPut\\' + molname +r'Maxtrace_withMolecules.png')
+
     ### max trace image is saved. ,molx/y contains the coordinates of possible molecules
 
-    get_mols_spectrum(data, reader, molx, moly)
+    reader = np.array(reader.read_all())
 
-    data=data.sort_values(by=['Frequency'])
-    plot_mols(data, molx.size)
+    get_mols_spectrum(data, reader, molx, moly , molname)
+
+    #data=data.sort_values(by=['Frequency'])
+    plot_mols(data, molx.size, molname)
+
+    fig, ax = plt.subplots(1)
+    plt.xlabel('Detuning (GHz)')
+    plt.ylabel('Fluorescence (A.U.)')
+    ax.plot((data.Frequency-data.Frequency.mean())/1e9 , data.mol8/mean_BG + (data.ElapsedTime//0.5))
+    #Extract_diffusion(data, 'mol0' , df= 1.5,dt= 0.3, molname= molname )
+
+    #butter_lowpass_filter(z[1], cutoff, fs, order = 5)
+
+
+
+
+
+
