3fbd01d39079bd3fe24e42512f8a5225add151d9
musavinejad <musavi.nezhad@yahoo.com>
2020-12-17 17:47:26 +0100

    Update main.py
    Smoothes Frequency_ Raw freq is kept in the last col

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from CryPy.SignalProcessing import *
 ########################################
 ## input documentation names :
 
-molname = 'M76876NC5'
+molname = 'M76824NC5_1V_3'
 
 #########################################
 
@@ -35,7 +35,7 @@ threshold = 1500
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    matplotlib.use('QT5Agg')
     folder= prepare_folders(MolNameTag = molname)
 
     name = select_data()  ## put the data you want to analyze in a folder named "...\Data\"
@@ -43,19 +43,20 @@ if __name__ == '__main__':
     #reader = np.array(read_camera(location=name + '_imagem.cam', load_all=True))  # read data as np.array
 
     data= pd.read_csv(name + '.dat', delimiter='\t', usecols=['DAQ_Index','ElapsedTime','Frequency'])
+    add_smothed_freq(data)
     mean_BG, STD_BG = measure_noise(reader)  # for the threshold in finding molecules
     max_img = get_Max_Trace(reader)
     ### plotting the max trace image and the molecules found in it.
     fig, ax = plt.subplots(1)
-    ax.imshow(max_img, vmin= mean_BG, vmax= mean_BG+10* STD_BG )
+    ax.imshow(max_img, vmin= mean_BG, vmax= 3*mean_BG)
     fig.suptitle(molname+ '_Max Trace')
 
     fig.savefig(molname + '_DataOutPut\\' + molname + r'MaxTrace.png' )
-    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=4* STD_BG)
+    molx, moly = find_mols(max_img, neighborhood_size=5, threshold=5* STD_BG)
     #molx[1] = 46
     #moly[1] = 81
-    #molx = np.array([82])
-    #moly = np.array([70])
+    # molx = np.array([54])
+    # moly = np.array([74])
     add_patches_to_img(ax, molx, moly)  ## Adds rectangles around the molecules
     fig.savefig(molname + '_DataOutPut\\' + molname +r'Maxtrace_withMolecules.png')
 
@@ -68,11 +69,12 @@ if __name__ == '__main__':
     #data=data.sort_values(by=['Frequency'])
     plot_mols(data, molx.size, molname)
 
-    fig, ax = plt.subplots(1)
-    plt.xlabel('Detuning (GHz)')
-    plt.ylabel('Fluorescence (A.U.)')
-    ax.plot((data.Frequency-data.Frequency.mean())/1e9 , data.mol8/mean_BG + (data.ElapsedTime//0.5))
-    #Extract_diffusion(data, 'mol0' , df= 1.5,dt= 0.3, molname= molname )
+    # fig, ax = plt.subplots(1)
+    # plt.xlabel('Detuning (GHz)')
+    # plt.ylabel('Fluorescence (A.U.)')
+    # ax.plot((data.Frequency-data.Frequency.mean())/1e9 , data.mol0/mean_BG + (data.ElapsedTime//0.5))
+
+    # Extract_diffusion(data.copy(), 'mol6' , df=0.5,dt=10.11, molname= molname, mean_BG=mean_BG)
 
     #butter_lowpass_filter(z[1], cutoff, fs, order = 5)
 
